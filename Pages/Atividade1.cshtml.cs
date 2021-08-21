9ccc080435b8c8b703a40f59371be61b7f9d52ff
Ciro Dourado <>
2021-08-21 00:19:57 -0300

    refatorando pages

--- Pages/Atividade1.cshtml.cs
+++ Pages/Atividade1.cshtml.cs
@@ -1,25 +1,53 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System;
+using ES;
 
-namespace aa_time_1.Pages
-{
-    public class Atividade1Model : PageModel
-    {
-        private readonly ILogger<Atividade1Model> _logger;
+namespace aa_time_1.Pages {
 
-        public Atividade1Model(ILogger<Atividade1Model> logger)
-        {
-            _logger = logger;
-        }
+public class Atividade1Model: PageModel {
+    private readonly ILogger<Atividade1Model> _logger;
+    public string TempoBinaria      { get; set; }
+    public string TempoBolha        { get; set; }
+    public string TempoFiltro       { get; set; }
+    public string TempoLinear       { get; set; }
+    public string InstrucoesBinaria { get; set; }
+    public string InstrucoesBolha   { get; set; }
+    public string InstrucoesFiltro  { get; set; }
+    public string InstrucoesLinear  { get; set; }
 
-        public void OnGet()
-        {
+    public Atividade1Model(ILogger<Atividade1Model> logger) {
+        _logger = logger;
+    } // new(args)
 
-        }
+    public void OnGet() {
+        string[] tempo   = {"Dados","Complexidade","Tempo","transferencias"};
+        var caminhoTempo = Diretorio.FormatarCaminho(tempo);
+        var tempoBinaria = new StreamReader($"{caminhoTempo}Binaria.json");
+        var tempoBolha   = new StreamReader($"{caminhoTempo}Bolha.json");
+        var tempoFiltro  = new StreamReader($"{caminhoTempo}Filtro.json");
+        var tempoLinear  = new StreamReader($"{caminhoTempo}Linear.json");
+
+        string[] instrucoes   = {"Dados","Complexidade","Instrucoes","transferencias"};
+        var caminhoInstrucoes = Diretorio.FormatarCaminho(instrucoes);
+        var instrucoesBinaria = new StreamReader($"{caminhoInstrucoes}Binaria.json");
+        var instrucoesBolha   = new StreamReader($"{caminhoInstrucoes}Bolha.json");
+        var instrucoesFiltro  = new StreamReader($"{caminhoInstrucoes}Filtro.json");
+        var instrucoesLinear  = new StreamReader($"{caminhoInstrucoes}Linear.json");
+
+        TempoBinaria      = tempoBinaria.ReadToEnd();
+        TempoBolha        = tempoBolha.ReadToEnd();
+        TempoFiltro       = tempoFiltro.ReadToEnd();
+        TempoLinear       = tempoLinear.ReadToEnd();
+        InstrucoesBinaria = instrucoesBinaria.ReadToEnd();
+        InstrucoesBolha   = instrucoesBolha.ReadToEnd();
+        InstrucoesFiltro  = instrucoesFiltro.ReadToEnd();
+        InstrucoesLinear  = instrucoesLinear.ReadToEnd();
     }
-}
+} // public class Atividade1Model: PageModel
+} // namespace aa_time_1.Pages
--- Pages/Atividade2.cshtml.cs
+++ Pages/Atividade2.cshtml.cs
@@ -1,25 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System;
+using ES;
 
-namespace aa_time_1.Pages
-{
-    public class Atividade2Model : PageModel
-    {
-        private readonly ILogger<Atividade2Model> _logger;
+namespace aa_time_1.Pages {
 
-        public Atividade2Model(ILogger<Atividade2Model> logger)
-        {
-            _logger = logger;
-        }
+public class Atividade2Model: PageModel {
+    private readonly ILogger<Atividade2Model> _logger;
+    public string TempoHashMd5    { get; set; }
+    public string TempoHashSqm    { get; set; }
+    public string TempoHashLinear { get; set; }
 
-        public void OnGet()
-        {
+    public Atividade2Model(ILogger<Atividade2Model> logger) {
+        _logger = logger;
+    } // new(args)
 
-        }
+    public void OnGet() {
+        string[] tempo   = {"Dados","Complexidade","Tempo","diretorio"};
+        var caminhoTempo = Diretorio.FormatarCaminho(tempo);
+        var tempoHashMd5    = new StreamReader($"{caminhoTempo}HashMd5.json");
+        var tempoHashSqm    = new StreamReader($"{caminhoTempo}HashSqm.json");
+        var tempoHashLinear = new StreamReader($"{caminhoTempo}Linear.json");
+
+        TempoHashMd5    = tempoHashMd5.ReadToEnd();
+        TempoHashSqm    = tempoHashSqm.ReadToEnd();
+        TempoHashLinear = tempoHashLinear.ReadToEnd();
     }
-}
+} // public class Atividade2Model: PageModel
+} // namespace aa_time_1.Pages
