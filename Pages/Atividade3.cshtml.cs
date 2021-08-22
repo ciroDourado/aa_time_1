20c245ae436a7a40e39a36f4c42ef5e6952f288c
Ciro Dourado <>
2021-08-21 22:01:25 -0300

    iniciando o envio do grafo ao front

--- Pages/Atividade3.cshtml.cs
+++ Pages/Atividade3.cshtml.cs
@@ -1,25 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System;
 
-namespace aa_time_1.Pages
-{
-    public class Atividade3Model : PageModel
-    {
-        private readonly ILogger<Atividade3Model> _logger;
+using Grafos;
+using ES;
 
-        public Atividade3Model(ILogger<Atividade3Model> logger)
-        {
-            _logger = logger;
-        }
+namespace aa_time_1.Pages {
 
-        public void OnGet()
-        {
+public class Atividade3Model: PageModel {
+    private readonly ILogger<Atividade3Model> _logger;
+    public string VerticesJson { get; set; }
+    public string ArestasJson  { get; set; }
 
-        }
+    public Atividade3Model(ILogger<Atividade3Model> logger) {
+        _logger = logger;
+    } // new(args)
+
+    public void OnGet() {
+        string[] aoArquivo = {"Dados", "Grafos", "olimpianos.txt"};
+        var olimpianosTxt  = Diretorio.FormatarCaminho(aoArquivo);
+        var genealogia  = new GrafoDirecionado();
+        var olimpianos  = Arquivo.LerLinhas(olimpianosTxt);
+
+        genealogia.Adicionar(olimpianos);
+        VerticesJson = genealogia.VerticesEmJson();
     }
-}
+} // public class Atividade3Model: PageModel
+} // namespace aa_time_1.Pages
