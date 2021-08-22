5a6811261cddba2062fc526c6bf7c9108ee68d7c
Ciro Dourado <>
2021-08-21 23:04:27 -0300

    testando envio de dados para front

--- Pages/Atividade3.cshtml.cs
+++ Pages/Atividade3.cshtml.cs
@@ -19,16 +19,31 @@ public class Atividade3Model: PageModel {
 
     public Atividade3Model(ILogger<Atividade3Model> logger) {
         _logger = logger;
+        VerticesJson = string.Empty;
+        ArestasJson  = string.Empty;
     } // new(args)
 
     public void OnGet() {
         string[] aoArquivo = {"Dados", "Grafos", "olimpianos.txt"};
         var olimpianosTxt  = Diretorio.FormatarCaminho(aoArquivo);
-        var genealogia  = new GrafoDirecionado();
-        var olimpianos  = Arquivo.LerLinhas(olimpianosTxt);
+        var olimpianos     = Arquivo.LerLinhas(olimpianosTxt);
 
+        var genealogia  = new GrafoDirecionado();
         genealogia.Adicionar(olimpianos);
+
+        aoArquivo = new string[]{"Dados", "Grafos", "parentescos.txt"};
+        var parentescosTxt = Diretorio.FormatarCaminho(aoArquivo);
+        var parentescos    = Arquivo.LerLinhas(parentescosTxt);
+
+        foreach (var parentesco in parentescos) {
+            var grau  = parentesco.Split(',');
+            var pai   = grau[0];
+            var filho = grau[1];
+            genealogia.Conectar(pai, filho);
+        }
+
         VerticesJson = genealogia.VerticesEmJson();
+        ArestasJson  = genealogia.ArestasEmJson();
     }
 } // public class Atividade3Model: PageModel
 } // namespace aa_time_1.Pages
