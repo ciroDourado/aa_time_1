cc2d271bb6fb4522b769f66d2b3368a965bad410
Ciro Dourado <>
2021-08-23 00:10:19 -0300

    refatorando construtor

--- Pages/Atividade3.cshtml.cs
+++ Pages/Atividade3.cshtml.cs
@@ -22,12 +22,14 @@ public class Atividade3Model: PageModel {
         _logger      = logger;
         ArestasJson  = string.Empty;
         VerticesJson = string.Empty;
-        genealogia   = InicializarGrafo();
+        genealogia   = new GrafoDirecionado();
+
+        AdicionarDeuses(genealogia);
+        AdicionarRelacoesHereditarias(genealogia);
+        AdicionarRelacoesConjugais(genealogia);
     } // new(args)
 
-    private GrafoDirecionado InicializarGrafo() {
-        var novoGrafo = new GrafoDirecionado();
-
+    private void AdicionarDeuses(GrafoDirecionado grafo) {
         string[] aoArquivo = {"Dados", "Grafos", "olimpianos.txt"};
         var olimpianosTxt  = Diretorio.FormatarCaminho(aoArquivo);
         var olimpianos     = Arquivo.LerLinhas(olimpianosTxt);
@@ -36,10 +38,12 @@ public class Atividade3Model: PageModel {
             var infos = olimpiano.Split(',');
             var nome  = infos[0];
             var sexo  = infos[1];
-            novoGrafo.Adicionar(new Vertice(nome, sexo));
+            grafo.Adicionar(new Vertice(nome, sexo));
         }
+    } // AdicionarDeuses
 
-        aoArquivo = new string[]{"Dados", "Grafos", "hereditariedades.txt"};
+    private void AdicionarRelacoesHereditarias(GrafoDirecionado grafo) {
+        string[] aoArquivo = {"Dados", "Grafos", "hereditariedades.txt"};
         var hereditariedadesTxt = Diretorio.FormatarCaminho(aoArquivo);
         var hereditariedades    = Arquivo.LerLinhas(hereditariedadesTxt);
         //
@@ -47,10 +51,12 @@ public class Atividade3Model: PageModel {
             var grau  = hereditariedade.Split(',');
             var pai   = grau[0];
             var filho = grau[1];
-            novoGrafo.Conectar(pai, filho, Familiar.Hereditariedade);
+            grafo.Conectar(pai, filho, Familiar.Hereditariedade);
         }
+    } // AdicionarRelacoesHereditarias
 
-        aoArquivo = new string[]{"Dados", "Grafos", "conjugalidades.txt"};
+    private void AdicionarRelacoesConjugais(GrafoDirecionado grafo) {
+        string[] aoArquivo = {"Dados", "Grafos", "conjugalidades.txt"};
         var conjugalidadesTxt = Diretorio.FormatarCaminho(aoArquivo);
         var conjugalidades    = Arquivo.LerLinhas(conjugalidadesTxt);
         //
@@ -58,12 +64,12 @@ public class Atividade3Model: PageModel {
             var papel  = conjugalidade.Split(',');
             var marido = papel[0];
             var mulher = papel[1];
-            novoGrafo.Conectar(marido, mulher, Familiar.Conjugalidade);
+            grafo.Conectar(marido, mulher, Familiar.Conjugalidade);
         }
-        return novoGrafo;
-    } // InicializarGrafo
+    } // AdicionarRelacoesConjugais
 
     public void OnGet() {
+        genealogia.ExibirArestas();
         // VerticesJson = genealogia.VerticesEmJson();
         // ArestasJson  = genealogia.ArestasEmJson();
     }
