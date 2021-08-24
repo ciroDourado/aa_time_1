e6f42c7189abd72bef9e97f7c17ae112043fc494
gabrielcancio <gabriel.cancio02@gmail.com>
2021-08-24 11:31:17 -0300

    Adicionando funcao de busca segundo a lib

--- wwwroot/js/atividade3.js
+++ wwwroot/js/atividade3.js
@@ -30,3 +30,17 @@ function setupDiagram(diagram, array, focusId) {
     diagram.select(node);
   }
 }
+
+function findMarriage(diagram, a, b) {  // A and B are node keys
+  var nodeA = diagram.findNodeForKey(a);
+  var nodeB = diagram.findNodeForKey(b);
+  if (nodeA !== null && nodeB !== null) {
+    var it = nodeA.findLinksBetween(nodeB);  // in either direction
+    while (it.next()) {
+      var link = it.value;
+      // Link.data.category === "Marriage" means it's a marriage relationship
+      if (link.data !== null && link.data.category === "Marriage") return link;
+    }
+  }
+  return null;
+}
