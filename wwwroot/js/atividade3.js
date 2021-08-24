6b36b95eddb9ee5a320f5ff8e161b021ce9c19f9
gabrielcancio <gabriel.cancio02@gmail.com>
2021-08-24 11:33:17 -0300

    Adicionando funcao setupParents segundo a lib

--- wwwroot/js/atividade3.js
+++ wwwroot/js/atividade3.js
@@ -94,3 +94,27 @@ function setupMarriages(diagram) {
     }
   }
 }
+
+// process parent-child relationships once all marriages are known
+function setupParents(diagram) {
+  var model = diagram.model;
+  var nodeDataArray = model.nodeDataArray;
+  for (var i = 0; i < nodeDataArray.length; i++) {
+    var data = nodeDataArray[i];
+    var key = data.key;
+    var mother = data.m;
+    var father = data.f;
+    if (mother !== undefined && father !== undefined) {
+      var link = findMarriage(diagram, mother, father);
+      if (link === null) {
+        // or warn no known mother or no known father or no known marriage between them
+        if (window.console) window.console.log("unknown marriage: " + mother + " & " + father);
+        continue;
+      }
+      var mdata = link.data;
+      var mlabkey = mdata.labelKeys[0];
+      var cdata = { from: mlabkey, to: key };
+      myDiagram.model.addLinkData(cdata);
+    }
+  }
+}
