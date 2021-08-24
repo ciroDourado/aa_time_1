4dfec2863840bb4965eff651d6965c4b502e17bd
gabrielcancio <gabriel.cancio02@gmail.com>
2021-08-24 11:29:44 -0300

    Adicionando funcao de setup segundo a lib

--- wwwroot/js/atividade3.js
+++ wwwroot/js/atividade3.js
@@ -6,4 +6,27 @@ const dados = JSON.parse(
     })
 );
 
-console.log(dados);
+
+// Funções de configuração do grafo segundo documentação da lib
+
+// create and initialize the Diagram.model given an array of node data representing people
+function setupDiagram(diagram, array, focusId) {
+  diagram.model =
+    go.GraphObject.make(go.GraphLinksModel,
+      { // declare support for link label nodes
+        linkLabelKeysProperty: "labelKeys",
+        // this property determines which template is used
+        nodeCategoryProperty: "s",
+        // if a node data object is copied, copy its data.a Array
+        copiesArrays: true,
+        // create all of the nodes for people
+        nodeDataArray: array
+      });
+  setupMarriages(diagram);
+  setupParents(diagram);
+
+  var node = diagram.findNodeForKey(focusId);
+  if (node !== null) {
+    diagram.select(node);
+  }
+}
