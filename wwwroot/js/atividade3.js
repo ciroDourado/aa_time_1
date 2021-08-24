f1f0749d0fee5888e72e2727d0538e484cc4894f
gabrielcancio <gabriel.cancio02@gmail.com>
2021-08-24 11:32:27 -0300

    Adicionando funcao setupMarriages segundo a lib

--- wwwroot/js/atividade3.js
+++ wwwroot/js/atividade3.js
@@ -44,3 +44,53 @@ function findMarriage(diagram, a, b) {  // A and B are node keys
   }
   return null;
 }
+
+// now process the node data to determine marriages
+function setupMarriages(diagram) {
+  var model = diagram.model;
+  var nodeDataArray = model.nodeDataArray;
+  for (var i = 0; i < nodeDataArray.length; i++) {
+    var data = nodeDataArray[i];
+    var key = data.key;
+    var uxs = data.ux;
+    if (uxs !== undefined) {
+      if (typeof uxs === "number") uxs = [uxs];
+      for (var j = 0; j < uxs.length; j++) {
+        var wife = uxs[j];
+        if (key === wife) {
+          // or warn no reflexive marriages
+          continue;
+        }
+        var link = findMarriage(diagram, key, wife);
+        if (link === null) {
+          // add a label node for the marriage link
+          var mlab = { s: "LinkLabel" };
+          model.addNodeData(mlab);
+          // add the marriage link itself, also referring to the label node
+          var mdata = { from: key, to: wife, labelKeys: [mlab.key], category: "Marriage" };
+          model.addLinkData(mdata);
+        }
+      }
+    }
+    var virs = data.vir;
+    if (virs !== undefined) {
+      if (typeof virs === "number") virs = [virs];
+      for (var j = 0; j < virs.length; j++) {
+        var husband = virs[j];
+        if (key === husband) {
+          // or warn no reflexive marriages
+          continue;
+        }
+        var link = findMarriage(diagram, key, husband);
+        if (link === null) {
+          // add a label node for the marriage link
+          var mlab = { s: "LinkLabel" };
+          model.addNodeData(mlab);
+          // add the marriage link itself, also referring to the label node
+          var mdata = { from: key, to: husband, labelKeys: [mlab.key], category: "Marriage" };
+          model.addLinkData(mdata);
+        }
+      }
+    }
+  }
+}
